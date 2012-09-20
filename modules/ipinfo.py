ad261665b209abe61bcc5c62bf2cf7eaa5da22b5
Sven Slootweg <jamsoftgamedev@gmail.com>
2012-09-21 01:36:25 +0200

    Add DNS record lookup

--- modules/ipinfo.py
+++ modules/ipinfo.py
@@ -10,7 +10,7 @@ donate something, even if just a little, have a look on my donation page
 over at http://cryto.net/~joepie91/donate.html - Thanks!
 """
 
-import subprocess, json, urllib
+import subprocess, json, urllib, re
 
 def ping(phenny, input):
 	try:
@@ -64,25 +64,86 @@ ping.priority = 'medium'
 ping.example = ".ping cryto.net"
 
 def lookup(phenny, input):
-	try:
-		ip = input.group(2)
-		data = json.loads(urllib.urlopen("http://ip-api.com/json/%s" % ip).read())
+	host = input.group(2)
+	
+	if re.match("[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}", host):
+		try:
+			ip = host
+			data = json.loads(urllib.urlopen("http://ip-api.com/json/%s" % ip).read())
+			
+			if data['status'] == "success":
+				if data['as'] == None:
+					data['as'] = "Unknown AS"
+					
+				phenny.say("IP: %s (%s)" % (ip, data['reverse']))
+				phenny.say("Location: %s, %s, %s (%s)" % (data['city'], data['regionName'], data['country'], data['countryCode']))
+				phenny.say("ISP: %s" % data['isp'])
+				phenny.say("Organization: %s (%s)" % (data['org'], data['as']))
+				phenny.say("Coordinates: latitude %s, longitude %s" % (data['lat'], data['lon']))
+			else:
+				phenny.say("IP-API indicated the request failed. Are you sure the IP you specified is valid?")
+			
+		except:
+			phenny.say("Could not look up IP. Either you specified an invalid IP, or IP-API is down.")
+			raise
+	else:
+		dig = subprocess.Popen(["dig", "any", host], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+		out, error = dig.communicate()
 		
-		if data['status'] == "success":
-			if data['as'] == None:
-				data['as'] = "Unknown AS"
+		records = {
+			"A": [],
+			"AAAA": [],
+			"MX": [],
+			"TXT": [],
+			"SPF": [],
+			"NS": [],
+			"SOA": [],
+			"CNAME": []
+		}
+		
+		for line in out.splitlines():
+			if line.startswith(";"):
+				pass
+			elif line.strip() == "":
+				pass
+			else:
+				parts = re.split("\s+", line)
 				
-			phenny.say("IP: %s (%s)" % (ip, data['reverse']))
-			phenny.say("Location: %s, %s, %s (%s)" % (data['city'], data['regionName'], data['country'], data['countryCode']))
-			phenny.say("ISP: %s" % data['isp'])
-			phenny.say("Organization: %s (%s)" % (data['org'], data['as']))
-			phenny.say("Coordinates: latitude %s, longitude %s" % (data['lat'], data['lon']))
-		else:
-			phenny.say("IP-API indicated the request failed. Are you sure the IP you specified is valid?")
+				try:
+					records[parts[3]].append(" ".join(parts[4:]))
+				except:
+					pass
 		
-	except:
-		phenny.say("Could not look up IP. Either you specified an invalid IP, or IP-API is down.")
-		raise
+		phenny.say("DNS records for %s:" % host)
+		
+		if len(records["SOA"]) > 0:
+			for record in records["SOA"]:
+				phenny.say("SOA: %s" % record)
+			
+		if len(records["NS"]) > 0:
+			phenny.say("NS: %s" % ", ".join(records["NS"]))
+		
+		if len(records["CNAME"]) > 0:
+			phenny.say("CNAME : %s" % ", ".join(records["CNAME"]))
+			
+		if len(records["A"]) > 0:
+			phenny.say("A: %s" % ", ".join(records["A"]))
+		
+		if len(records["AAAA"]) > 0:
+			phenny.say("AAAA: %s" % ", ".join(records["AAAA"]))
+		
+		if len(records["MX"]) > 0:
+			phenny.say("MX: %s" % ", ".join(records["MX"]))
+		
+		if len(records["TXT"]) > 0:
+			for record in records["TXT"]:
+				phenny.say("TXT: %s" % record)
+		
+		if len(records["SPF"]) > 0:
+			for record in records["SPF"]:
+				phenny.say("SPF: %s" % record)
+		
+		
 		
 lookup.commands = ['lookup']
 lookup.priority = 'medium'
