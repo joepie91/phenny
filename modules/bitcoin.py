97ac7aee21ea793200b3ae5fa259dace81c9c716
Sven Slootweg <jamsoftgamedev@gmail.com>
2013-04-09 22:01:05 +0200

    Add simple module for returning the current Bitcoin exchange rate

---
+++ modules/bitcoin.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding=utf-8
+"""
+bitcoin.py - Phenny Bitcoin exchange rate module
+Copyright 2012, Sven Slootweg (joepie91), cryto.net
+Licensed under the WTFPL.
+
+I live off donations entirely. If you appreciate this module and wish to
+donate something, even if just a little, have a look on my donation page
+over at http://cryto.net/~joepie91/donate.html - Thanks!
+"""
+
+import requests, json
+
+def getrate(phenny, input):
+	response = requests.get("http://blockchain.info/ticker")
+	data = json.loads(response.text)
+	
+	phenny.say("$1 = %.2f BTC, €1 = %.2f BTC" % (data["USD"]["15m"], data["EUR"]["15m"]))
+	
+getrate.commands = ['bitcoin']
+getrate.priority = 'high'
+getrate.example = ".bitcoin"
