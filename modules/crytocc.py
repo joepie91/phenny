9a58ac81fdd918ddc7e3106a95773c19afb080c7
Sven Slootweg <jamsoftgamedev@gmail.com>
2013-11-04 21:07:49 +0100

    Add channel welcoming module

---
+++ modules/crytocc.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding=utf-8
+"""
+bitcoin.py - Phenny channel welcoming module
+Copyright 2012, Sven Slootweg (joepie91), cryto.net
+Licensed under the WTFPL.
+
+I live off donations entirely. If you appreciate this module and wish to
+donate something, even if just a little, have a look on my donation page
+over at http://cryto.net/~joepie91/donate.html - Thanks!
+"""
+
+import requests, json
+
+def welcome(phenny, input):
+	phenny.say("%s: welcome to %s! Please be aware that this channel is publicly logged, and make sure to read the rules in the channel topic. You may hide messages from the public logs by prefixing them with [off]." % (input.group(1), sender))
+	
+welcome.commands = ['welcome']
+welcome.priority = 'high'
+welcome.example = ".welcome joepie91"
